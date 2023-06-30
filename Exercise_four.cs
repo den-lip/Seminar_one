83db9ea496b22ba72799271aa024a2e5b17e9a88
den-lip <136602716+den-lip@users.noreply.github.com>
2023-06-30 15:25:48 +0300

    Написали программу, находящую все четные числа, до числа N

---
+++ Exercise_four.cs
@@ -0,0 +1,8 @@
+// Задание 4
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=number;i++)
+{
+if (i % 2 == 0)
+System.Console.WriteLine("Четные числа " + i );
+}
